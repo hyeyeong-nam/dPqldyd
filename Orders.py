18fade9ebe60f38e82dd48196e74f87fb93f128e
clip968 <clip968@gmail.com>
2021-12-11 17:21:22 +0900

    Update Orders.py

--- Orders.py
+++ Orders.py
@@ -5,21 +5,29 @@ class Order:
         self.Menu_price = [3000, 3500, 2700, 4000]
         self.Side_price = [3000, 3200, 3400, 4200]
         self.coffee_menu = ["아메리카노", "카페 라떼", "카푸치노", "바닐라 라떼"]
-        self.Menu_menu=["레모네이드", "초코 라떼"]
+        self.Menu_menu = ["레모네이드", "초코 라떼", "아이스티", "딸기 스무디"]
+        self.side_menu = ["초코 케이크", "딸기 케이크", "티라미수", "샌드위치"]
         self.total_price = 0
         self.ordered = ''
         
     def guess_menu(self):
+        
         if self.ordered in self.coffee_menu:
             menu_index = self.coffee_menu.index(self.ordered)
             menu_price = self.coffee_price[menu_index]
             
         elif self.ordered in self.Menu_menu:
             menu_index = self.Menu_menu.index(self.ordered)
+            menu_price = self.Menu_price[menu_index]
+            
+        elif self.ordered in self.side_menu:
+            menu_index = self.side_menu.index(self.ordered)
             menu_price = self.Side_price[menu_index]
+            
         return menu_price
             
     def add_order(self,order):
+        
         menu = [item['Menu'] for item in self.menu_list]
         self.ordered = order
         if self.ordered not in menu:
